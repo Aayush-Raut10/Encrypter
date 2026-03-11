8064f8b799e09abd09e1e08a1dc650aebf17070a
Aayush-Raut10 <aayushraut209@gmail.com>
2026-03-11 20:47:11 +0545

    Initial commit

---
+++ web-app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {"message":"welcome to encrypter"}
+
+
+@app.post('/encrypt/ceaser-cipher')
+def ceaser_cipher(payload: dict = Body(...)):
+
+    key = payload.get("key")
+    plain_text = payload.get("plain_text").upper()
+
+    cipher_text = ""
+    
+    for char in plain_text:
+        position = ord(char) - 65
+        
+        letter = chr((position + key) + 65 )
+        
+        cipher_text += letter
+    
+    return {"cipher_text":cipher_text}
